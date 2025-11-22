592ac3409f2c6b7a7dae95543a5a5bea16634610
polekaleksandra <aleksandrapolek2@wp.pl>
2025-11-22 23:59:48 +0100

    poprawki

--- brick.h
+++ brick.h
@@ -1,51 +1,47 @@
-#include <SFML/Graphics.hpp> 
+#ifndef BRICK_H
+#define BRICK_H
+#pragma once
+
+#include <SFML/Graphics.hpp>
 #include <array>
-class Brick :public sf::RectangleShape {
+
+class Brick : public sf::RectangleShape {
 private:
-	int punktyZycia;
-	bool jestZniszczony;
-	static const std::array<sf::Color, 4> colorLUT;
+    int punktyZycia;
+    bool jestZniszczony;
+    static const std::array<sf::Color, 4> colorLUT;
+
 public:
-	Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L);
-	void aktualizujKolor();
-	void trafienie();
-	void draw(sf::RenderTarget& window);
-	bool czyZniszczony() const { return jestZniszczony; } 
+    Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
+        punktyZycia = L;
+        jestZniszczony = false;
+        this->setPosition(startPo);
+        this->setSize(rozmiar);
+        this->setOutlineThickness(2.f);
+        this->setOutlineColor(sf::Color::White);
+        aktualizujKolor();
+    }
+
+    void aktualizujKolor() {
+        if (punktyZycia >= 0 && punktyZycia < static_cast<int>(colorLUT.size())) {
+            this->setFillColor(colorLUT[punktyZycia]);
+        }
+    }
+
+    void trafienie() {
+        if (jestZniszczony == true) return;
+        punktyZycia--;
+        aktualizujKolor();
+        if (punktyZycia <= 0) jestZniszczony = true;
+    }
+
+    void draw(sf::RenderTarget& window) {
+        if (!jestZniszczony) {
+            window.draw(*this);
+        }
+    }
+
+    bool czyZniszczony() const { return jestZniszczony; }
 };
 
-const std::array<sf::Color, 4> Brick::colorLUT = {
-	sf::Color::Transparent,
-	sf::Color::Red,          
-	sf::Color::Yellow,
-	sf::Color::Blue,
-};
-
-Brick::Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
-	punktyZycia = L;
-	jestZniszczony = false;
-	this->setPosition(startPo); 
-	this->setSize(rozmiar);     
-	this->setOutlineThickness(2.f); 
-	this->setOutlineColor(sf::Color::White);
-	aktualizujKolor(); 
-};
-
-void Brick::trafienie() {
-	if (jestZniszczony == true)
-		return;
-	punktyZycia--;
-	aktualizujKolor();
-	if (punktyZycia <= 0)
-		jestZniszczony = true;
-}
-
-void Brick::aktualizujKolor() {
-	if (punktyZycia >= 0 && punktyZycia < static_cast<int>(colorLUT.size())) 
-		this->setFillColor(colorLUT[punktyZycia]);
-}
-
-void Brick::draw(sf::RenderTarget& window) {
-	if (!jestZniszczony) { 
-		window.draw(*this);
-	}
-}
+#endif
