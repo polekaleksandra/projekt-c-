1ab17614d20823d876943979ed629b9f9fe6ddae
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 18:56:29 +0100

    Refactor Brick color handling and initialization

--- brick1.cpp
+++ brick1.cpp
@@ -1,8 +1,38 @@
 #include "brick.h"
+#include <array>
 
-const std::array<sf::Color, 4> Brick::colorLUT = {
-    sf::Color::Transparent,
-    sf::Color::Red,
-    sf::Color::Yellow,
-    sf::Color::Blue,
+
+const std::array<sf::Color, 4> colorLUT = {
+    sf::Color(0, 0, 0, 0),       
+    sf::Color(255, 0, 0),        
+    sf::Color(255, 255, 0),      
+    sf::Color(0, 0, 255)         
 };
+
+Brick::Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
+    punktyZycia = L;
+    jestZniszczony = false;
+    this->setPosition(startPo);
+    this->setSize(rozmiar);
+    this->setOutlineThickness(2.f);
+    this->setOutlineColor(sf::Color::White);
+    aktualizujKolor();
+}
+
+void Brick::aktualizujKolor() {
+    if (punktyZycia >= 0 && punktyZycia < static_cast<int>(colorLUT.size())) {
+        this->setFillColor(colorLUT[punktyZycia]);
+    }
+    else {
+        this->setFillColor(sf::Color::White);
+    }
+}
+
+void Brick::trafienie() {
+    if (jestZniszczony == true)
+        return;
+    punktyZycia--;
+    aktualizujKolor();
+    if (punktyZycia <= 0)
+        jestZniszczony = true;
+}
