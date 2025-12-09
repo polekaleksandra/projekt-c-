91feca0ba1eebd64463445f9627ce90d12e26426
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 19:01:32 +0100

    Refactor Brick class constructor and methods

--- brick.h
+++ brick.h
@@ -3,45 +3,27 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <array>
 
 class Brick : public sf::RectangleShape {
 private:
     int punktyZycia;
     bool jestZniszczony;
-    static const std::array<sf::Color, 4> colorLUT;
 
 public:
-    Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
-        punktyZycia = L;
-        jestZniszczony = false;
-        this->setPosition(startPo);
-        this->setSize(rozmiar);
-        this->setOutlineThickness(2.f);
-        this->setOutlineColor(sf::Color::White);
-        aktualizujKolor();
-    }
+   
+    Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L);
 
-    void aktualizujKolor() {
-        if (punktyZycia >= 0 && punktyZycia < static_cast<int>(colorLUT.size())) {
-            this->setFillColor(colorLUT[punktyZycia]);
-        }
-    }
-
-    void trafienie() {
-        if (jestZniszczony == true) return;
-        punktyZycia--;
-        aktualizujKolor();
-        if (punktyZycia <= 0) jestZniszczony = true;
-    }
-
-    void draw(sf::RenderTarget& window) {
-        if (!jestZniszczony) {
-            window.draw(*this);
-        }
-    }
+    void aktualizujKolor();
+    void trafienie();
 
     bool czyZniszczony() const { return jestZniszczony; }
+    int getHP() const { return punktyZycia; }
+    float getX() const { return getPosition().x; }
+    float getY() const { return getPosition().y; }
+
+    sf::FloatRect getGlobalBounds() const {
+        return sf::RectangleShape::getGlobalBounds();
+    }
 };
 
 #endif
