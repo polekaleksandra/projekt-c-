bcc93558fcceb7c06eaeb0ced12fe3be36dea5de
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 20:19:29 +0100

    Refactor Ball class to support multiple shapes

--- ball.h
+++ ball.h
@@ -1,88 +1,42 @@
-
 #ifndef BALL_H
 #define BALL_H
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <cmath>
-#include <iostream>
-#include "paddle.h"
+#include "paddle.h" 
+
+enum class BallType {
+    Circle,
+    Star,
+    Heart
+};
 
 class Pilka {
 private:
     float x, y;
     float vx, vy;
     float radius;
-    sf::CircleShape shape;
+
+    sf::CircleShape circleShape;
+    sf::ConvexShape starShape;
+    sf::ConvexShape heartShape;
+
+    BallType currentType;
 
 public:
-    Pilka(float startX, float startY, float velX, float velY, float r)
-        : x(startX), y(startY), vx(velX), vy(velY), radius(r) {
-        shape.setRadius(radius);
-        shape.setOrigin(radius, radius);
-        shape.setPosition(x, y);
-        shape.setFillColor(sf::Color::White);
-    }
+    Pilka(float startX, float startY, float velX, float velY, float r);
 
-    void move() {
-        x += vx;
-        y += vy;
-        shape.setPosition(x, y);
-    }
+    void move();
+    void bounceX();
+    void bounceY();
+    void collideWalls(float width, float height);
+    bool collidePaddle(const Paletka& p);
+    void setState(float newX, float newY, float newVx, float newVy);
 
-    void bounceX() {
-        vx = -vx;
-    }
+    void setType(BallType type);
 
-    void bounceY() {
-        vy = -vy;
-    }
-
-    void collideWalls(float width, float height) {
-        if (x - radius <= 0) {
-            x = radius;
-            bounceX();
-        }
-        if (x + radius >= width) {
-            x = width - radius;
-            bounceX();
-        }
-        if (y - radius <= 0) {
-            y = radius;
-            bounceY();
-        }
-        shape.setPosition(x, y);
-    }
-
-    bool collidePaddle(const Paletka& p) {
-        sf::FloatRect paddleBounds = p.getGlobalBounds();
-        sf::FloatRect ballBounds = getGlobalBounds();
-
-        if (ballBounds.intersects(paddleBounds)) {
-            vy = -std::abs(vy);
-            y = paddleBounds.top - radius;
-            shape.setPosition(x, y);
-            return true;
-        }
-        return false;
-    }
-
-    sf::FloatRect getGlobalBounds() const {
-        return sf::FloatRect(x - radius, y - radius, 2 * radius, 2 * radius);
-    }
-
-    
-    void setState(float newX, float newY, float newVx, float newVy) {
-        x = newX;
-        y = newY;
-        vx = newVx;
-        vy = newVy;
-        shape.setPosition(x, y);
-    }
-
-    void draw(sf::RenderTarget& target) {
-        target.draw(shape);
-    }
+    void draw(sf::RenderTarget& target) const;
+    sf::FloatRect getGlobalBounds() const;
 
     float getX() const { return x; }
     float getY() const { return y; }
