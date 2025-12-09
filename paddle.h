cbf4d774cc95493299346da9d64f15bc860cb476
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 20:18:59 +0100

    Refactor Paletka class member declarations and methods

--- paddle.h
+++ paddle.h
@@ -6,53 +6,22 @@
 
 class Paletka {
 private:
-    float x;
-    float y;
-    float szerokosc;
-    float wysokosc;
+    float x, y;
+    float szerokosc, wysokosc;
     float predkosc;
     sf::RectangleShape shape;
 
 public:
-    Paletka(float startX, float startY, float sw, float wy, float speed)
-        : x(startX), y(startY), szerokosc(sw), wysokosc(wy), predkosc(speed) {
-        shape.setSize(sf::Vector2f(szerokosc, wysokosc));
-        shape.setOrigin(szerokosc / 2.f, wysokosc / 2.f);
-        shape.setPosition(x, y);
-        shape.setFillColor(sf::Color::Green);
-    }
-
-    void moveLeft() {
-        x -= predkosc;
-        shape.setPosition(x, y);
-    }
-
-    void moveRight() {
-        x += predkosc;
-        shape.setPosition(x, y);
-    }
-
-    void clampToBounds(float width) {
-        float half = szerokosc / 2.f;
-        if (x - half < 0) x = half;
-        if (x + half > width) x = width - half;
-        shape.setPosition(x, y);
-    }
-
     
-    void setPosition(float newX, float newY) {
-        x = newX;
-        y = newY;
-        shape.setPosition(x, y);
-    }
+    Paletka(float startX, float startY, float sw, float wy, float speed);
 
-    sf::FloatRect getGlobalBounds() const {
-        return shape.getGlobalBounds();
-    }
+    void moveLeft();
+    void moveRight();
+    void clampToBounds(float width);
+    void setPosition(float newX, float newY);
 
-    void draw(sf::RenderTarget& target) {
-        target.draw(shape);
-    }
+    void draw(sf::RenderTarget& target);
+    sf::FloatRect getGlobalBounds() const;
 
     float getX() const { return x; }
     float getY() const { return y; }
