75c792a412b75fbc368a2c63c7306c9495122bd1
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 19:00:54 +0100

    Modify menu items and adjust positions
    Updated menu items and positions for better layout.

--- menu.cpp
+++ menu.cpp
@@ -4,33 +4,46 @@
 Menu::Menu(float width, float height) {
     
     if (!font.loadFromFile("arial.ttf")) {
+        std::cout << "Nie mozna zaladowac czcionki arial.ttf\n";
         
         if (!font.loadFromFile("C:/Windows/Fonts/arial.ttf")) {
+            std::cout << "Nie mozna zaladowac czcionki z Windows Fonts\n";
             
-            std::cout << "Uzywam domyslnej czcionki SFML\n";
-            font.loadFromFile(""); 
+            if (!font.loadFromFile("")) {
+                std::cout << "Nie mozna zaladowac domyslnej czcionki SFML\n";
+            }
         }
     }
 
-    menuItems.resize(3);
+    menuItems.resize(4);
 
+   
     menuItems[0].setFont(font);
     menuItems[0].setFillColor(sf::Color::Red);
     menuItems[0].setString("Nowa gra");
     menuItems[0].setCharacterSize(30);
-    menuItems[0].setPosition(sf::Vector2f(width / 2 - 80, height / 4 * 1));
+    menuItems[0].setPosition(sf::Vector2f(width / 2 - 80, height / 5 * 1));
 
+    
     menuItems[1].setFont(font);
     menuItems[1].setFillColor(sf::Color::White);
-    menuItems[1].setString("Wyniki");
+    menuItems[1].setString("Wczytaj gre");
     menuItems[1].setCharacterSize(30);
-    menuItems[1].setPosition(sf::Vector2f(width / 2 - 50, height / 4 * 2));
+    menuItems[1].setPosition(sf::Vector2f(width / 2 - 80, height / 5 * 2));
 
+   
     menuItems[2].setFont(font);
     menuItems[2].setFillColor(sf::Color::White);
-    menuItems[2].setString("Wyjscie");
+    menuItems[2].setString("Wyniki");
     menuItems[2].setCharacterSize(30);
-    menuItems[2].setPosition(sf::Vector2f(width / 2 - 50, height / 4 * 3));
+    menuItems[2].setPosition(sf::Vector2f(width / 2 - 50, height / 5 * 3));
+
+   
+    menuItems[3].setFont(font);
+    menuItems[3].setFillColor(sf::Color::White);
+    menuItems[3].setString("Wyjscie");
+    menuItems[3].setCharacterSize(30);
+    menuItems[3].setPosition(sf::Vector2f(width / 2 - 50, height / 5 * 4));
 
     selectedItemIndex = 0;
 }
