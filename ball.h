28010abab38196371e1ad859ceb0da91da0c98ba
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 18:55:40 +0100

    Refactor ball class methods and improve collision detection

--- ball.h
+++ ball.h
@@ -1,5 +1,7 @@
+
 #ifndef BALL_H
 #define BALL_H
+#pragma once
 
 #include <SFML/Graphics.hpp>
 #include <cmath>
@@ -15,8 +17,7 @@ private:
 
 public:
     Pilka(float startX, float startY, float velX, float velY, float r)
-        : x(startX), y(startY), vx(velX), vy(velY), radius(r)
-    {
+        : x(startX), y(startY), vx(velX), vy(velY), radius(r) {
         shape.setRadius(radius);
         shape.setOrigin(radius, radius);
         shape.setPosition(x, y);
@@ -29,51 +30,54 @@ public:
         shape.setPosition(x, y);
     }
 
-    void bounceX() { vx = -vx; }
-    void bounceY() { vy = -vy; }
+    void bounceX() {
+        vx = -vx;
+    }
+
+    void bounceY() {
+        vy = -vy;
+    }
 
     void collideWalls(float width, float height) {
         if (x - radius <= 0) {
             x = radius;
             bounceX();
         }
-
         if (x + radius >= width) {
             x = width - radius;
             bounceX();
         }
-
         if (y - radius <= 0) {
             y = radius;
             bounceY();
         }
-
         shape.setPosition(x, y);
     }
 
     bool collidePaddle(const Paletka& p) {
-        float palX = p.getX();
-        float palY = p.getY();
-        float palW = p.getSzerokosc();
-        float palH = p.getWysokosc();
+        sf::FloatRect paddleBounds = p.getGlobalBounds();
+        sf::FloatRect ballBounds = getGlobalBounds();
 
-        bool overlapX = (x >= palX - palW / 2.f) && (x <= palX + palW / 2.f);
-        float top = palY - palH / 2.f;
-
-        bool overlapY =
-            (y + radius >= top) &&
-            (y - radius < top);
-
-        if (overlapX && overlapY) {
+        if (ballBounds.intersects(paddleBounds)) {
             vy = -std::abs(vy);
-            y = top - radius;
+            y = paddleBounds.top - radius;
             shape.setPosition(x, y);
             return true;
         }
         return false;
     }
+
     sf::FloatRect getGlobalBounds() const {
         return sf::FloatRect(x - radius, y - radius, 2 * radius, 2 * radius);
+    }
+
+    
+    void setState(float newX, float newY, float newVx, float newVy) {
+        x = newX;
+        y = newY;
+        vx = newVx;
+        vy = newVy;
+        shape.setPosition(x, y);
     }
 
     void draw(sf::RenderTarget& target) {
