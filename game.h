e616f9964bf2212bbb5f9d8b840691716f7567e0
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 20:22:59 +0100

    Refactor game.h to include string and update formatting

--- game.h
+++ game.h
@@ -4,30 +4,44 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <string>
 #include "paddle.h"
-#include "ball.h"
-#include "brick.h"  
+#include "ball.h"  
+#include "brick.h"
 #include "GameState.h"
 
 class Game {
 public:
     Game();
+
     void update();
     void render(sf::RenderWindow& window);
+
     bool isGameOver() const { return m_gameOver; }
     void reset();
+
+    void setBallType(BallType type);
+
+    int getScore() const { return m_score; }
+
     void captureState(GameState& state) const;
     bool loadState(const GameState& state);
 
 private:
     const float WIDTH = 800.f;
     const float HEIGHT = 600.f;
+
     Paletka m_paletka;
     Pilka m_pilka;
     std::vector<Brick> m_bloki;
+
+    sf::Font m_font;
+    sf::Text m_scoreText;
+    sf::Text m_comboText;
+
+    int m_score;
+    int m_comboMultiplier; 
     bool m_gameOver = false;
-    const float BRICK_WIDTH = (800.f - (8 - 1) * 2.f) / 8;
-    const float BRICK_HEIGHT = 25.f;
 };
 
 #endif
