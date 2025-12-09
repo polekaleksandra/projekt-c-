4e3301ec29625e32491a47d83c52286e8b42cd14
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-09 20:22:14 +0100

    Refactor Brick color handling and outline properties

--- brick1.cpp
+++ brick1.cpp
@@ -1,8 +1,6 @@
 #include "brick.h"
-#include <array>
 
-
-const std::array<sf::Color, 4> colorLUT = {
+const std::array<sf::Color, 4> Brick::colorLUT = {
     sf::Color(0, 0, 0, 0),       
     sf::Color(255, 0, 0),        
     sf::Color(255, 255, 0),      
@@ -14,23 +12,22 @@ Brick::Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
     jestZniszczony = false;
     this->setPosition(startPo);
     this->setSize(rozmiar);
-    this->setOutlineThickness(2.f);
-    this->setOutlineColor(sf::Color::White);
+    this->setOutlineThickness(1.f);
+    this->setOutlineColor(sf::Color::Black);
     aktualizujKolor();
 }
 
 void Brick::aktualizujKolor() {
-    if (punktyZycia >= 0 && punktyZycia < static_cast<int>(colorLUT.size())) {
+    if (punktyZycia > 0 && punktyZycia < static_cast<int>(colorLUT.size())) {
         this->setFillColor(colorLUT[punktyZycia]);
     }
     else {
-        this->setFillColor(sf::Color::White);
+        this->setFillColor(colorLUT[0]); 
     }
 }
 
 void Brick::trafienie() {
-    if (jestZniszczony == true)
-        return;
+    if (jestZniszczony) return;
     punktyZycia--;
     aktualizujKolor();
     if (punktyZycia <= 0)
