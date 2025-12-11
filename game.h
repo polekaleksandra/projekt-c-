354bdb7947a3db4536b4171909d3310bead28db7
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-11 20:42:09 +0100

    Update game.h

--- game.h
+++ game.h
@@ -9,30 +9,37 @@
 #include "brick.h"
 #include "GameState.h"
 
+// Główna klasa gry zarządzająca logiką
 class Game {
 public:
     Game();
 
     void update();
     void render(sf::RenderWindow& window);
+
     bool isGameOver() const { return m_gameOver; }
-    void reset();
+    void reset(); // Restart gry i generowanie poziomu
+
     void setBallType(BallType type);
     int getScore() const { return m_score; }
+
     void captureState(GameState& state) const;
     bool loadState(const GameState& state);
 
 private:
     const float WIDTH = 800.f;
     const float HEIGHT = 600.f;
+
     Paletka m_paletka;
     Pilka m_pilka;
-    std::vector<Brick> m_bloki;
+    std::vector<Brick> m_bloki; // Kontener na cegły
+
     sf::Font m_font;
     sf::Text m_scoreText;
     sf::Text m_comboText;
+
     int m_score;
-    int m_comboMultiplier; 
+    int m_comboMultiplier; // Mnożnik punktów za serię
     bool m_gameOver = false;
 };
 
