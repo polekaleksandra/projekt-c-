1563acc72f2bd027353744d9481e3693ddb62f72
polekaleksandra <aleksandrapolek2@wp.pl>
2025-12-11 20:34:30 +0100

    Update brick1.cpp

--- brick1.cpp
+++ brick1.cpp
@@ -1,11 +1,12 @@
 #include "brick.h"
 
+// Definicja kolorów dla poziomów życia 
 const std::array<sf::Color, 5> Brick::colorLUT = {
-    sf::Color(0, 0, 0, 0),       
-    sf::Color(255, 0, 0),        
-    sf::Color(255, 255, 0),      
-    sf::Color(0, 0, 255),        
-    sf::Color(128, 0, 128)       
+    sf::Color(0, 0, 0, 0),
+    sf::Color(255, 0, 0),
+    sf::Color(255, 255, 0),
+    sf::Color(0, 0, 255),
+    sf::Color(128, 0, 128)
 };
 
 Brick::Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
@@ -19,19 +20,19 @@ Brick::Brick(sf::Vector2f startPo, sf::Vector2f rozmiar, int L) {
 }
 
 void Brick::aktualizujKolor() {
-    
+    // Ustawia kolor na podstawie HP, zabezpieczenie przed wyjściem poza tablicę
     if (punktyZycia > 0 && punktyZycia < static_cast<int>(colorLUT.size())) {
         this->setFillColor(colorLUT[punktyZycia]);
     }
     else {
-        this->setFillColor(colorLUT[0]);
+        this->setFillColor(colorLUT[0]); // Niewidoczny
     }
 }
 
 void Brick::trafienie() {
     if (jestZniszczony) return;
     punktyZycia--;
-    aktualizujKolor(); 
+    aktualizujKolor(); // Zmiana koloru po trafieniu
     if (punktyZycia <= 0)
         jestZniszczony = true;
 }
